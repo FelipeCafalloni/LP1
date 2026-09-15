79561ff306cf0bbf6ceb24e5b5fe1c0bac375656
FelipeCafalloni <147274578+FelipeCafalloni@users.noreply.github.com>
2026-09-15 12:42:17 -0300

    Atividade 06/10
    Exercício de calculo do preço para uma viagem, estrutura feita, falta programar o calculo.

---
+++ Combustivel_Interface/src/main/java/org/example/CombustivelController.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CombustivelController {
+
+    @FXML
+    private Label result;
+
+    @FXML
+    private TextField txtcombust;
+
+    @FXML
+    private TextField txtdist;
+
+    @FXML
+    void Calcular(ActionEvent event) {
+
+    }
+
+}
