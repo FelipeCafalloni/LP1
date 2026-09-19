75e34814be6ff71091a8b508f63ec0498cbf6408
FelipeCafalloni <147274578+FelipeCafalloni@users.noreply.github.com>
2026-09-19 12:58:58 -0300

    Atividades 06/10
    Atividade do Aluguel concluída

--- Aluguel_Interface/src/main/java/org/example/AluguelController.java
+++ Aluguel_Interface/src/main/java/org/example/AluguelController.java
@@ -20,6 +20,19 @@ public class AluguelController {
     void Calcular(ActionEvent event) {
     int dias = Integer.parseInt(txtdias.getText());
     double km = Double.parseDouble(txtkm.getText());
+    double diaria = 95;
+    double km_ideal = 100;
+    double km_dia = km/dias;
+    double aluguel;
+    if (km_dia>100){
+        aluguel= (diaria*dias)+((km_dia-km_ideal)*0.1);
+        String resultado = String.format("%.2f", aluguel);
+        result.setText("O valor do aluguel ficou: " + resultado);
+    } else {
+        aluguel = (diaria*dias);
+        String resultado = String.format("%.2f", aluguel);
+        result.setText("O valor do aluguel ficou: " + resultado);
+    }
     }
 
 }
