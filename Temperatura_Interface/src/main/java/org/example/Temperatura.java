ea2bf19077ccd8ee97edde005dad6cf0f36a63ce
FelipeCafalloni <147274578+FelipeCafalloni@users.noreply.github.com>
2026-09-21 09:21:04 -0300

    Atividade 06/10
    Exercício da temperatura concluído

---
+++ Temperatura_Interface/src/main/java/org/example/Temperatura.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Temperatura {
+
+    int clima;
+
+    public Temperatura(int Clima){
+        int clima = Clima;
+    }
+
+    public Temperatura(){
+
+    }
+
+    public int getClima() {
+        return clima;
+    }
+
+    public void setClima(int clima) {
+        this.clima = clima;
+    }
+}
